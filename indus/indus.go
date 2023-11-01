88ab19145d9e039bcccd6883b0796c8ac629736d
Eric Fischer <efischer78@yahoo.fr>
2023-11-01 12:12:37 +0100

    ok

--- indus/indus.go
+++ indus/indus.go
@@ -92,9 +92,9 @@ func buildFor(ctx context.Context, commit string, release string, goos string, g
 	binFile.Dir().EnsureEmpty()
 	ldflagsRelease := ""
 	if release != "" {
-		ldflagsRelease = fmt.Sprintf(" -X util.GitRelease=%s", release)
+		ldflagsRelease = fmt.Sprintf(" -X 'util.GitRelease=%s'", release)
 	}
-	ldflags := fmt.Sprintf("-X util.GitCommit=%s%s", commit, ldflagsRelease)
+	ldflags := fmt.Sprintf("-X 'util.GitCommit=%s'%s", commit, ldflagsRelease)
 	aCmd := exec.CommandContext(ctx, "go", "build", "-ldflags", ldflags, "-gcflags", "all=-N -l", "-o", binFile.String(), fmt.Sprintf("%s/%s", newfs.CWD(), srcMainPath))
 	aCmd.Env = environmentFor(goos, goarch)
 	aCmd.Stderr = os.Stderr
