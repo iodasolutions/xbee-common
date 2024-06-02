5780959f5e114ea2de18707dc1f46c14fe79c717
Eric Fischer <efischer78@yahoo.fr>
2024-06-02 23:40:33 +0200

    ok

--- util/debug.go
+++ util/debug.go
@@ -2,13 +2,7 @@ package util
 
 import "github.com/iodasolutions/xbee-common/cmd"
 
-var debugOption = cmd.NewBooleanOption("debug", "", false)
-
-func init() {
-	cmd.Register(debugOption)
-}
-
-func Debug() bool { return debugOption.BooleanValue() }
+func Debug() bool { return Contains(cmd.XbeeFlags, "--xbeeDebug") }
 func WithDelve() []string {
 	return []string{"--listen=:40000", "--headless=true",
 		"--api-version=2", "--accept-multiclient", "--check-go-version=false", "exec", "--"}
