b50fd0ff619e287de26c9d77a80118127c1706ef
Eric Fischer <efischer78@yahoo.fr>
2024-07-13 09:17:25 +0200

    ok

--- provider/instanceinfo.go
+++ provider/instanceinfo.go
@@ -1,5 +1,11 @@
 package provider
 
+import (
+	"bytes"
+	"github.com/iodasolutions/xbee-common/cmd"
+	"github.com/iodasolutions/xbee-common/template"
+)
+
 type InstanceInfo struct {
 	Name          string `json:"name,omitempty"`
 	State         string `json:"state,omitempty"`
@@ -10,4 +16,20 @@ type InstanceInfo struct {
 	User          string `json:"user,omitempty"`
 	PackIdExist   bool   `json:"packidexist,omitempty"`
 	SystemIdExist bool   `json:"systemidexist,omitempty"`
+}
+
+func (info *InstanceInfo) hostnameScript() string {
+	model := map[string]interface{}{
+		"name": info.Name,
+	}
+	script := `cat > /etc/hostname <<EOF
+{{ .name }}
+EOF
+hostname {{ .name }}
+`
+	w := &bytes.Buffer{}
+	if err := template.OutputWithTemplate(script, w, model, nil); err != nil {
+		panic(cmd.Error("failed to parse network template : %v", err))
+	}
+	return w.String()
 }
